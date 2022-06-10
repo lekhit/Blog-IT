83e7c118135ce62d6edd00288fb3f1e5d454b9db
lekhit <blekhit@gmail.com>
2022-06-10 20:43:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs-rywtem
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-rywtem)
---
+++ pages/blog/[sno].js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {useRouter} from 'next/router'
+
+const Post = () => {
+  const router=useRouter();
+  const {sno}=router.query;
+  return (
+    <div>Post: {sno}</div>
+  )
+}
+
+export default Post
